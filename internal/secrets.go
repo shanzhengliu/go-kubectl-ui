f522cfcc57d27f41701a50a971eb7b7c8943c074
shanzheng liu <liushanzheng960522@163.com>
2024-04-17 10:00:27 +1000

    secrect go done

--- internal/secrets.go
+++ internal/secrets.go
@@ -2,7 +2,9 @@ package internal
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	apiv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -13,19 +15,49 @@ type Secret struct {
 	Namespace string `json:"namespace"`
 }
 
-func SecretList(clientset *kubernetes.Clientset, namespace string) []Secret {
-	secretListClient := clientset.CoreV1().Secrets(namespace)
-	secret, error := secretListClient.List(context.TODO(), apiv1.ListOptions{})
+func SecrectList(clientset *kubernetes.Clientset, namespace string) []Secrect {
+	secrectListClient := clientset.CoreV1().Secrets(namespace)
+	secrects, error := secrectListClient.List(context.TODO(), apiv1.ListOptions{})
 	if error != nil {
 		fmt.Println(error.Error())
 	}
-	secretList := []Secret{}
-	for _, item := range secret.Items {
-		secret := &Secret{
+	secrectList := []Secret{}
+	for _, item := range secrects.Items {
+		secrect := &Secret{
 			Name:      item.Name,
 			Namespace: item.Namespace,
 		}
-		secretList = append(secretList, *secret)
+		secrectList = append(secrectList, *secrect)
 	}
-	return secretList
+	return secrectList
+}
+
+func SecretDetail(clientset *kubernetes.Clientset, namespace string, name string) map[string]string {
+	secretListClient := clientset.CoreV1().Secrets(namespace)
+	secret, err := secretListClient.Get(context.TODO(), name, apiv1.GetOptions{})
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+
+	decodedData := make(map[string]string)
+	for key, value := range secret.Data {
+		decodedData[key] = string(value)
+	}
+
+	return decodedData
+}
+
+func SecretListHandler(w http.ResponseWriter, r *http.Request) {
+	ctxMap := r.Context().Value("map").(map[string]interface{})
+	clientset := ctxMap["clientSet"].(*kubernetes.Clientset)
+	result := SecrectList(clientset, ctxMap["namespace"].(string))
+	ReturnTypeHandler(r.Context(), result, w, r)
+}
+
+func SecretDetailHandler(w http.ResponseWriter, r *http.Request) {
+	ctxMap := r.Context().Value("map").(map[string]interface{})
+	clientset := ctxMap["clientSet"].(*kubernetes.Clientset)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(SecretDetail(clientset, ctxMap["namespace"].(string), r.URL.Query().Get("secrect")))
 }
